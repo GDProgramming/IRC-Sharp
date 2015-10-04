8d7689afaee712288951824507303f580ff0e627
nirkbirk <nirkbirk@googlemail.com>
2015-10-04 23:49:43 +0100

    Functioning IRC Chat!
    Connected IRC Client stuff to Tommy's work on the UI
    
    Need to handle disconnects, etc. now, but it's a start

--- src/MainWindow.xaml.cs
+++ src/MainWindow.xaml.cs
@@ -24,16 +24,17 @@ namespace IRC_Sharp
     /// </summary>
     public partial class MainWindow : Window
     {
+        IRC irc;
+        string channel = "#IRCSharpTest";
+        string nick = "IRCSharp";
         public MainWindow()
         {
             InitializeComponent();
 
-            IRC irc = ((App)Application.Current).Irc; //get irc instance
+            irc = ((App)Application.Current).Irc; //get irc instance
 
             string serverUrl = "irc.freenode.net";
             int port = 6667;
-
-            string channel = "#IRCSharpTest";
 
             irc.Client.OnConnected += (sender, args) =>
             {
@@ -42,6 +43,11 @@ namespace IRC_Sharp
                 irc.Client.Login("IRCSharp", "IRCSharp", 0, "IrcSharp");
                 irc.Client.RfcJoin(channel);
 
+                //Show we've logged into the channel
+                PrimaryTab.Header = "<" + channel + ">";
+                UpdateMessageBoxContent("You have successfully logged into " + channel);
+
+                //Start listening to IRC server responses -- DONE ON A BACKGROUND WORKER
                 irc.StartListenerThread();
             };
 
@@ -58,19 +64,53 @@ namespace IRC_Sharp
             
         }
 
+        protected void SendMessage()
+        {
+            //Grab the content from the text box
+            string msg = txtMessage.Text;
+            //Send the message (Allow SmartIrc to handle formatting)
+            irc.Client.SendMessage(SendType.Message, channel, msg);
+            //Update Textbox content with the message after we format it
+            string formattedMsg = string.Format("<{0}> {1}", nick, msg);
+            UpdateMessageBoxContent(formattedMsg);
+
+            //Finally, clear the message box content, since we're done sending.
+            txtMessage.Clear();
+        }
+
+        protected void UpdateMessageBoxContent(string message)
+        {
+            txtboxChat.Text += Environment.NewLine + message;
+        }
+
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+                SendMessage();
+        }
+
+        //The following handlers are invoked by the background worker thread, so we need to use Dispatcher to modify the UI.
+        //TODO: Reroute to necessary tab, based on the incoming message's channel (e.Data.Channel)
         void OnChannelMessage(object sender, IrcEventArgs e)
         {
+            string msg = string.Format("<{0}> {1}", e.Data.Nick, @e.Data.Message);
             Dispatcher.BeginInvoke(new Action(() => 
             {
-                lblMessage.Content = string.Format("{0}:\n({1}) <{2}> {3}", e.Data.Type, e.Data.Channel, e.Data.Nick, @e.Data.Message);
+                UpdateMessageBoxContent(msg);
             }));
         }
 
         void OnQueryMessage(object sender, IrcEventArgs e)
         {
+            string msg = string.Format("(private) <{0}> {1}", e.Data.Nick, @e.Data.Message);
             Dispatcher.BeginInvoke(new Action(() =>
             {
-                lblMessage.Content = string.Format("{0}:\n(private) <{1}> {2}", e.Data.Type, e.Data.Nick, @e.Data.Message);
+                UpdateMessageBoxContent(msg);
             }));
         }
     }
